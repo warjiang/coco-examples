be2c8c7ca759e42468193cd1cb3d474dd73fa0dd
warjiang <1096409085@qq.com>
2026-01-05 11:02:50 +0800

    feat: add qdrant client dependencies and example scripts
    - Add qdrant-client and volcengine-python-sdk as dependencies
    - Include example scripts for creating collections and querying points
    
    feat: initialize coco-examples python project
    
    add main.py with hello world example
    configure pyproject.toml with basic project metadata
    include standard python gitignore file
    
    refactor(qdrant-examples): use environment variables for configuration
    
    Replace hardcoded values with environment variables for Qdrant URL, collection name, and other settings to improve configurability and security
    
    feat(qdrant-examples): add new example scripts for image search and embedding
    
    - Add 03.py script for creating Qdrant collection and embedding images
    - Add 04.py script for interactive image search using Qdrant and Ark
    - Update .gitignore to include .env files
    - Add dotenv dependency to pyproject.toml and uv.lock

---
+++ qdrant-examples/01.py
@@ -0,0 +1,41 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+from qdrant_client.models import PointStruct
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+DEFAULT_QDRANT_URL = os.environ.get("QDRANT_URL", "your_qdrant_endpoint")
+client = QdrantClient(url=DEFAULT_QDRANT_URL)
+
+client.create_collection(
+    collection_name="test_collection",
+    vectors_config=VectorParams(size=4, distance=Distance.DOT),
+)
+
+operation_info = client.upsert(
+    collection_name="test_collection",
+    wait=True,
+    points=[
+        PointStruct(id=1, vector=[0.05, 0.61, 0.76, 0.74], payload={"city": "Berlin"}),
+        PointStruct(id=2, vector=[0.19, 0.81, 0.75, 0.11], payload={"city": "London"}),
+        PointStruct(id=3, vector=[0.36, 0.55, 0.47, 0.94], payload={"city": "Moscow"}),
+        PointStruct(id=4, vector=[0.18, 0.01, 0.85, 0.80], payload={"city": "New York"}),
+        PointStruct(id=5, vector=[0.24, 0.18, 0.22, 0.44], payload={"city": "Beijing"}),
+        PointStruct(id=6, vector=[0.35, 0.08, 0.11, 0.44], payload={"city": "Mumbai"}),
+    ],
+)
+
+print(operation_info)
+
+
+search_result = client.query_points(
+    collection_name="test_collection",
+    query=[0.2, 0.1, 0.9, 0.7],
+    with_payload=False,
+    limit=3
+).points
+
+print(search_result)
